78fa299a8717beb85fe408622f931cd0f705620c
korov <korov9@163.com>
2019-11-06 17:04:20 +0800

    remove aspectj

--- Java/SpringBoot/springboot/src/main/java/com/korov/springboot/aspect/aspectj/AOPConfig.java
+++ Java/SpringBoot/springboot/src/main/java/com/korov/springboot/aspect/aspectj/AOPConfig.java
@@ -1,9 +1,13 @@
 package com.korov.springboot.aspect.aspectj;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportResource;
 
+/**
+ * 1.安装按aspectj，确保IDE支持aspectj
+ * 2.添加aspectj的jar包：aspectjweaver，aspectjrt
+ * 3.在idea中需要设置Java Compiler为Ajc，并设置aspectj的jar包位置
+ */
 @Configuration//作为配置文件之一
-@ImportResource("classpath:aop/aop.xml")
+//@ImportResource("classpath:aop/aop.xml")
 public class AOPConfig {
 }
