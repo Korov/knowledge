9c73b1d53fda57c00e4765ea07ff5365c26ef1cd
korov9 <korov9@163.com>
2019-12-09 18:53:50 +0800

    add note about springmvc and test

--- Java/Gradle/knowledge/src/main/java/com/korov/gradle/knowledge/controller/NerfortuneController.java
+++ Java/Gradle/knowledge/src/main/java/com/korov/gradle/knowledge/controller/NerfortuneController.java
@@ -3,9 +3,11 @@ package com.korov.gradle.knowledge.controller;
 import com.korov.gradle.knowledge.model.Managers;
 import com.korov.gradle.knowledge.service.ManagersService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @RestController
@@ -16,5 +18,11 @@ public class NerfortuneController {
     @GetMapping(value = "/selectAll")
     public List<Managers> selectAll() {
         return service.selectAll();
+    }
+
+    @GetMapping(value = "/getRequest")
+    public void getRequest(HttpServletRequest request){
+        System.out.println(request.toString());
+        System.out.println("debug");
     }
 }
