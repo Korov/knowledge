819af9ef79f8c904a1dd914f45d1465061562cc6
korov9 <korov9@163.com>
2019-12-13 05:16:51 +0800

    add mianshi

--- Java/Gradle/knowledge/src/test/java/com/korov/gradle/knowledge/MyTest.java
+++ Java/Gradle/knowledge/src/test/java/com/korov/gradle/knowledge/MyTest.java
@@ -1,9 +1,6 @@
 package com.korov.gradle.knowledge;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class MyTest {
     public static void main(String[] args) {
@@ -11,12 +8,23 @@ public class MyTest {
         map.put("1", "a");
         map.put("2", "b");
         map.put("3", "c");
-        List<String> stringList = new ArrayList<>();
-        stringList.add(null);
-        stringList.add(null);
-        stringList.add(null);
         for (Map.Entry<String, String> entry : map.entrySet()) {
             System.out.println(entry.getKey() + ":" + entry.getValue());
         }
+
+        List<String> stringList = new ArrayList<>();
+        stringList.add("a");
+        stringList.add("v");
+        stringList.add("s");
+        stringList.stream().forEach(System.out::println);
+        Iterator iterable = stringList.iterator();
+        while (iterable.hasNext()) {
+            iterable.next();
+            iterable.remove();
+            iterable.remove();
+        }
+        stringList.stream().forEach(System.out::println);
+        Collections.unmodifiableList(stringList);
+        Collections.sy
     }
 }
