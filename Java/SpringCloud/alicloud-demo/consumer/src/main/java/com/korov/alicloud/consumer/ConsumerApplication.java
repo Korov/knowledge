e328dce0661edb26427e8ca20044253afd9ee88e
korov <korov9@163.com>
2019-12-26 22:18:16 +0800

    添加关于SpringCloud nacos的demo

---
+++ Java/SpringCloud/alicloud-demo/consumer/src/main/java/com/korov/alicloud/consumer/ConsumerApplication.java
@@ -0,0 +1,45 @@
+package com.korov.alicloud.consumer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ConsumerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerApplication.class, args);
+    }
+
+    @LoadBalanced
+    @Bean(name = "rest")
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @RestController
+    public class TestController {
+
+        @Autowired
+        @Qualifier(value = "rest")
+        private final RestTemplate restTemplate;
+
+        public TestController(RestTemplate restTemplate) {
+            this.restTemplate = restTemplate;
+        }
+
+        @RequestMapping(value = "/echo1/{str}", method = RequestMethod.GET)
+        public String echo(@PathVariable String str) {
+            return restTemplate.getForObject("http://service-provider/echo/" + str, String.class);
+        }
+    }
+}
