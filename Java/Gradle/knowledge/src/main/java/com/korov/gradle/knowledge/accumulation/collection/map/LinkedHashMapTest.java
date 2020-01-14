10fb18643e9d9047abd61eb375d2c0eb8785e9dd
korov9 <korov9@163.com>
2020-01-14 13:24:31 +0800

    使用linkedhashmap实现LRU算法

--- Java/Gradle/knowledge/src/main/java/com/korov/gradle/knowledge/accumulation/collection/map/LinkedHashMapTest.java
+++ Java/Gradle/knowledge/src/main/java/com/korov/gradle/knowledge/accumulation/collection/map/LinkedHashMapTest.java
@@ -4,7 +4,7 @@ package com.korov.gradle.knowledge.accumulation.collection.map;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-public class mytest {
+public class LinkedHashMapTest {
     public static void main(String[] args) {
         //设置为true表示按照访问顺序排序，讲最近访问的元素移动到列表末尾
         Map<String, String> map = new LinkedHashMap<String, String>(20, 0.7F, true);
