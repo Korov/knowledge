58cf046ec78136590484d968796fa607b173d395
korov9 <korov9@163.com>
2020-01-17 19:18:54 +0800

    jvm面试题再次整理

--- Java/SpringBoot/springboot/src/test/java/com/korov/springboot/BaseTest/VoidTest.java
+++ Java/SpringBoot/springboot/src/test/java/com/korov/springboot/BaseTest/VoidTest.java
@@ -4,6 +4,7 @@ import com.korov.springboot.util.RegexUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
+import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
@@ -15,22 +16,7 @@ public class VoidTest {
 
     @Test
     public void test() throws Exception {
-        String value = "董事，董事长代理";
-
-        boolean isMatch = RegexUtil.isPartMatch("首席|长", value);
-        System.out.println(isMatch);
-
-        List<String> values = new ArrayList<>();
-        values.add("aaat");
-        values.add("aaa2s");
-        values.add("aaa3");
-        values.sort(new Comparator<String>() {
-            @Override
-            public int compare(String o1, String o2) {
-                return o1.compareTo(o2);
-            }
-        });
-        System.out.println("debug");
+        ByteBuffer buffer = ByteBuffer.allocateDirect(10 * 1024 * 1024);
     }
 
 
