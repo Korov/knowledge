84da6b65cb64919c140240a987e837c9f8cccd0f
korov9 <korov9@163.com>
2020-07-24 09:35:33 +0800

    增加Flink的学习模块

---
+++ Java/Flink/flink/example/src/main/java/org/flink/example/java/streaming/WordWithCount.java
@@ -0,0 +1,13 @@
+package org.flink.example.java.streaming;
+
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+/**
+ * 单词计数之滑动窗口计算
+ */
+public class WordWithCount {
+    public static void main(String[] args) {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.createLocalEnvironment();
+
+    }
+}
---
+++ Java/Flink/flink/example/src/main/scala/org/flink/example/scala/batch/SocketWindowWordCount.scala
@@ -0,0 +1,2 @@
+package org.flink.example.scala.batch
+
