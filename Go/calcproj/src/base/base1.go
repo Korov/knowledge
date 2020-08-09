4b7bf783e8ad93101515681bf95c37e2a1863995
korov9 <korov9@163.com>
2020-08-09 23:40:34 +0800

    go file

---
+++ Go/calcproj/src/base/base1.go
@@ -0,0 +1,43 @@
+package base
+
+import "fmt"
+
+func base1(value int) {
+	/**
+	var v1 int
+	var v2 string
+	var v3 [10]int //数组
+	var v4 []int //数组切片
+	var v5 struct{
+		f int
+	}
+	var v6 *int // 指针
+	var v7 map[string]int // map, key为string类型，value为int类型
+	var v8 func(a int) int
+
+	// 同时声明多个变量
+	var (
+		v9 int
+		v10 string
+	)
+	*/
+
+	/**
+	变量初始化
+	var v1 int = 10
+	var v2 = 10
+	v3 := 10
+	*/
+	switch value {
+	case 0:
+		fmt.Printf("Value:%d", value)
+	case 2:
+		fmt.Printf("Value:%d", value)
+	case 3:
+		fallthrough
+	case 4:
+		fmt.Printf("Value:%d", value)
+	default:
+		fmt.Printf("Value:%d", 0)
+	}
+}
---
+++ Go/first-project/detail/base_test.go
@@ -0,0 +1,7 @@
+package detail
+
+import "testing"
+
+func TestBase1(t *testing.T) {
+	base1(1)
+}
