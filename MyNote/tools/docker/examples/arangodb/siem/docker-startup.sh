8d08abf002be8874860690cc42c96a64fc3decda
Korov <korov9@163.com>
2020-09-17 09:17:04 +0800

    arangodb集群部署

--- MyNote/tools/docker/examples/arangodb/siem/docker-startup.sh
+++ MyNote/tools/docker/examples/arangodb/siem/docker-startup.sh
@@ -4,6 +4,8 @@
 -c /arango/arangodb3-3.6.6/config/agent.conf \
 --javascript.startup-directory /arango/arangodb3-3.6.6/usr/share/arangodb3/js \
 --javascript.app-path /arango/arangodb3-3.6.6/usr/share/arangodb3/js/apps \
+--log.file /var/lib/arangodb3/agency.log \
+--log.force-direct false \
 --server.endpoint tcp://0.0.0.0:8531 \
 --agency.my-address tcp://127.0.0.1:8531 \
 --server.authentication false \
@@ -17,6 +19,8 @@
 -c /arango/arangodb3-3.6.6/config/arangodb.conf \
 --javascript.startup-directory /arango/arangodb3-3.6.6/usr/share/arangodb3/js \
 --javascript.app-path /arango/arangodb3-3.6.6/usr/share/arangodb3/js/apps \
+--log.file /var/lib/arangodb3/dbserver1.log \
+--log.force-direct false \
 --server.authentication=false \
 --server.endpoint tcp://0.0.0.0:8526 \
 --cluster.my-address tcp://127.0.0.1:8526 \
@@ -28,6 +32,8 @@
 -c /arango/arangodb3-3.6.6/config/arangodb.conf \
 --javascript.startup-directory /arango/arangodb3-3.6.6/usr/share/arangodb3/js \
 --javascript.app-path /arango/arangodb3-3.6.6/usr/share/arangodb3/js/apps \
+--log.file /var/lib/arangodb3/dbserver2.log \
+--log.force-direct false \
 --server.authentication=false \
 --server.endpoint tcp://0.0.0.0:8527 \
 --cluster.my-address tcp://127.0.0.1:8527 \
@@ -39,6 +45,8 @@
 -c /arango/arangodb3-3.6.6/config/coordinator.conf \
 --javascript.startup-directory /arango/arangodb3-3.6.6/usr/share/arangodb3/js \
 --javascript.app-path /arango/arangodb3-3.6.6/usr/share/arangodb3/js/apps \
+--log.file /var/lib/arangodb3/coordinator.log \
+--log.force-direct false \
 --server.endpoint tcp://0.0.0.0:8529 \
 --cluster.my-address tcp://127.0.0.1:8529 \
 --server.authentication false \
