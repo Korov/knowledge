1c3e1eadc8055287ba04c8df93194cadba2c0ef0
korov9 <korov9@163.com>
2020-10-06 20:14:44 +0800

    完成插入排序

--- Python/algorithm/src/insertion_sort.py
+++ Python/algorithm/src/insertion_sort.py
@@ -1,8 +1,18 @@
 from loguru import logger
 
-def insertion_sort(values):
-    result = 0
-    for value in values:
-        result += value
 
-    return result
+def insertion_sort(values):
+    logger.info(values)
+    i = 1
+    for i in range(len(values)):
+        j = i - 1
+        while j >= 0:
+            if values[j] > values[j + 1]:
+                key = values[j + 1]
+                values[j + 1] = values[j]
+                values[j] = key
+                j -= 1
+            else:
+                break
+        i += 1
+    logger.info(values)
--- Python/algorithm/test/test_insertion_sort.py
+++ Python/algorithm/test/test_insertion_sort.py
@@ -1,7 +1,13 @@
 from src.insertion_sort import insertion_sort
+from loguru import logger
+import time
 
 
 def test_insertion_sort():
-    values = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    result = insertion_sort(values)
-    assert result == 45
+    values = [1, 2, 3, 9, 5, 6, 7, 8, 4]
+    start = time.time()
+    insertion_sort(values)
+    logger.info('insertion sort cost: %s second'%(time.time() - start))
+    assert values == [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
