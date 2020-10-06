9d8fe6eb942f4fa48e5b2a9e46f164aac135d7b9
korov9 <korov9@163.com>
2020-10-06 21:35:54 +0800

    修改python代码格式

--- Python/algorithm/test/test_insertion_sort.py
+++ Python/algorithm/test/test_insertion_sort.py
@@ -7,7 +7,5 @@ def test_insertion_sort():
     values = [1, 2, 3, 9, 5, 6, 7, 8, 4]
     start = time.time()
     insertion_sort(values)
-    logger.info('insertion sort cost: %s second'%(time.time() - start))
+    logger.info('insertion sort cost: %s second' % (time.time() - start))
     assert values == [1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-
