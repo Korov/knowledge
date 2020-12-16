3a3f085f6c18844d330d89a0026a98559980a653
korov9 <korov9@163.com>
2020-12-16 23:50:13 +0800

    update python log formate

--- Python/algorithm/src/base/list.py
+++ Python/algorithm/src/base/list.py
@@ -7,9 +7,9 @@ from src.log import log
 
 def get_movies():
     movie = ["The Holy Grail", "The Life of Brian", "The Meaning of Life"]
-    log.info('get index 1 %s' % (movie[1]))
+    log.info('get index 1 :%s' % (movie[1]))
     for value in movie:
-        log.info('get value %s' % (value))
+        log.info('get value :%s' % (value))
 
     # 扩展数据
     movie.extend(["value1", "value2"])
@@ -18,6 +18,6 @@ def get_movies():
     log.info('pop %s %s' % (movie.pop(), movie.pop()))
 
     movie.insert(0, "insert0")
-    log.info('all movie %s' % (movie))
+    log.info('all movie %s' % movie)
     movie.remove("insert0")
     return movie
