96a837a197ba7289c95741a2993820de84677c8a
korov9 <korov9@163.com>
2020-12-21 01:18:57 +0800

    update spider

--- Python/scrapy/tutorial/tutorial/spiders/quotes_spider.py
+++ Python/scrapy/tutorial/tutorial/spiders/quotes_spider.py
@@ -7,7 +7,6 @@ class QuotesSpider(scrapy.Spider):
     def start_requests(self):
         urls = [
             'http://quotes.toscrape.com/page/1/',
-            'http://quotes.toscrape.com/page/2/',
         ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
@@ -19,8 +18,18 @@ class QuotesSpider(scrapy.Spider):
                 'author': quote.css('small.author::text').get(),
                 'tags': quote.css('div.tags a.tag::text').getall(),
             }
-        page = response.url.split("/")[-2]
-        filename = f'quotes-{page}.html'
-        # with open(filename, 'wb') as f:
-        #     f.write(response.body)
-        self.log(f'Saved file {filename}')
+        next_page = response.css('li.next a::attr(href)').get()
+        if next_page is not None:
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback=self.parse)
+
+        for href in response.css('ul.pager a::attr(href)'):
+            yield response.follow(href, callback=self.parse)
+
+        for a in response.css('ul.pager a'):
+            yield response.follow(a, callback=self.parse)
+
+        anchors = response.css('ul.pager a')
+        yield from response.follow_all(anchors, callback=self.parse)
+
+        yield from response.follow_all(css='ul.pager a', callback=self.parse)
