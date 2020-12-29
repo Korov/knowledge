6cba18c33ce01c707fa0e71ec955c7fa75ebe2d7
korov9 <korov9@163.com>
2020-12-29 11:24:26 +0800

    function update

--- MyNote/Database/MySQL/function_and_producer/function_threat_ip_match.sql
+++ MyNote/Database/MySQL/function_and_producer/function_threat_ip_match.sql
@@ -52,6 +52,59 @@ end
 $$
 delimiter ;
 
+delimiter $$
+DROP function if exists siem_match_threat_ip;
+CREATE function siem_match_threat_ip(delimiter_value varchar(5), ip1 text, ip2 text) returns boolean
+    Deterministic
+begin
+    DECLARE ip_count1 INT default 0;
+    DECLARE ip_count2 INT default 0;
+    DECLARE join_count INT default 0;
+
+    if (delimiter_value is null) then
+        set delimiter_value = ',';
+    end if;
+
+    if (ip1 is null and ip2 is null) then
+        return true;
+    end if;
+
+    if ((ip1 is null and ip2 is not null) or (ip1 is not null and ip2 is null)) then
+        return false;
+    end if;
+
+    set ip_count1 = length(ip1) - length(replace(ip1, delimiter_value, '')) + 1;
+    set ip_count2 = length(ip2) - length(replace(ip2, delimiter_value, '')) + 1;
+
+    if (ip_count1 != ip_count2) THEN
+        return false;
+    else
+        if ip_count1 = 1 then
+            return substring_index(ip1, ':', 1) = substring_index(ip2, ':', 1);
+        end if;
+
+        select count(*)
+        into join_count
+        from (SELECT substring_index(substring_index(substring_index(a.ip, delimiter_value, b.help_topic_id + 1),
+                                                     delimiter_value, - 1), ':', 1) AS ips
+              FROM (select ip1 as ip) a
+                       JOIN mysql.help_topic b ON b.help_topic_id < ip_count1) ips1
+                 join (SELECT substring_index(
+                                      substring_index(substring_index(a.ip, delimiter_value, b.help_topic_id + 1),
+                                                      delimiter_value, - 1), ':', 1) AS ips
+                       FROM (select ip2 as ip) a
+                                JOIN mysql.help_topic b ON b.help_topic_id < ip_count2) ips2
+                      on ips1.ips = ips2.ips;
+        if join_count = ip_count1 then
+            return true;
+        else
+            return false;
+        end if;
+    end if;
+end
+$$
+delimiter ;
+
 
 delimiter $$
 DROP function if exists match_threat_port;
