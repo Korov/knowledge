3661ba59b64b4f4c06f6954eea888dfbb028cad8
korov <korov9@163.com>
2021-01-24 12:42:11 +0800

    camk更新

--- MyNote/tools/docker/examples/cmak/docker-entrypoint.sh
+++ MyNote/tools/docker/examples/cmak/docker-entrypoint.sh
@@ -1,5 +1,6 @@
-rm /opt/cmak-3.0.0.5/RUNNING_PID
+#!/bin/bash
 
 sed -i 's/localhost:2181/$ZK_HOSTS/g' /opt/cmak-3.0.0.5/conf/application.conf
 
-/opt/cmak-3.0.0.5/bin/cmak
+ln -s /opt/cmak-3.0.0.5/bin/cmak /usr/local/bin/cmak
+cmak
