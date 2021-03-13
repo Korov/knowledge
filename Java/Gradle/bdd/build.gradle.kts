5e4b5250a44c93e73adcc22f3bf886f33a6fd682
korov <korov9@163.com>
2021-03-14 03:07:37 +0800

    demo about curator

--- Java/Gradle/bdd/build.gradle.kts
+++ Java/Gradle/bdd/build.gradle.kts
@@ -28,6 +28,11 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.apache.curator:curator-framework:5.1.0")
     implementation("com.google.guava:guava:30.1-jre")
+
+    implementation("org.apache.logging.log4j:log4j-api:2.14.1")
+    implementation("org.apache.logging.log4j:log4j-core:2.14.1")
+    implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.14.1")
+
     testCompileOnly(platform("org.spockframework:spock-bom:2.0-M4-groovy-3.0"))
     testCompileOnly("org.spockframework:spock-core")
     testCompileOnly("org.junit.platform:junit-platform-launcher:1.8.0-M1")
---
+++ Java/Gradle/bdd/src/main/java/org/korov/java/ZookeeperUtil.java
@@ -0,0 +1,45 @@
+package org.korov.java;
+
+import org.apache.curator.RetryPolicy;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.framework.api.CreateBuilder;
+import org.apache.curator.framework.api.DeleteBuilder;
+import org.apache.curator.framework.api.ExistsBuilder;
+import org.apache.curator.framework.api.GetDataBuilder;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.apache.zookeeper.data.Stat;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.nio.charset.StandardCharsets;
+
+public class ZookeeperUtil {
+    private static final Logger logger = LoggerFactory.getLogger(ZookeeperUtil.class);
+
+    public static void main(String[] args) throws Exception {
+        RetryPolicy retryPolicy = new ExponentialBackoffRetry(1000, 3);
+        CuratorFramework client = CuratorFrameworkFactory.newClient("localhost:2181", 5000, 3000, retryPolicy);
+        client.start();
+
+        ExistsBuilder existsBuilder = client.checkExists();
+        if (existsBuilder.forPath("/curator-test") != null) {
+            DeleteBuilder deleteBuilder = client.delete();
+            deleteBuilder.forPath("/curator-test");
+        }
+
+        CreateBuilder createBuilder = client.create();
+        createBuilder.forPath("/curator-test", "demo".getBytes(StandardCharsets.UTF_8));
+
+        GetDataBuilder getDataBuilder = client.getData();
+        logger.info(new String(getDataBuilder.forPath("/curator-test"), StandardCharsets.UTF_8));
+        System.out.println(new String(getDataBuilder.forPath("/curator-test"), StandardCharsets.UTF_8));
+        Stat stat = null;
+        if ((stat = existsBuilder.forPath("/curator-test")) != null) {
+            logger.info("version:{}", stat.getVersion());
+            System.out.println(String.format("version:%s", stat.getVersion()));
+        }
+
+        client.close();
+    }
+}
