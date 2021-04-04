387f735375d8307fd3668c9142571360c3c715fb
korov <korov9@163.com>
2021-04-04 18:49:38 +0800

    修改go项目

--- Go/algorithm/go.mod
+++ Go/algorithm/go.mod
@@ -1,9 +1,9 @@
 module algorithm
 
-go 1.15
+go 1.16
 
 require (
 	github.com/cihub/seelog v0.0.0-20170130134532-f561c5e57575
-	github.com/onsi/ginkgo v1.14.1
+	github.com/onsi/ginkgo v1.16.0
 	github.com/onsi/gomega v1.10.1
 )
--- Go/algorithm/src/heap_sort_test.go
+++ Go/algorithm/src/heap_sort_test.go
@@ -2,28 +2,49 @@ package src_test
 
 import (
 	. "algorithm/src"
-	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/gomega"
+	"fmt"
+	"testing"
 )
 
-var _ = Describe("Merge Sort Test", func() {
-	Describe("ReturnResult", func() {
-		Context("default", func() {
-			var (
-				input  []int
-				result []int
-			)
-			BeforeEach(func() {
-				println("BeforeEach in Context")
-				input = []int{1, 3, 2, 6, 5, 4}
-				result = []int{1, 2, 3, 4, 5, 6}
-			})
-			It("heap_sort", func() {
-				v, _ := HeapSort(input)
-				Logger.Info("result:", v)
-				Logger.Flush()
-				Expect(v).To(Equal(result))
-			})
-		})
-	})
-})
+/**
+自带的单元测试框架
+ */
+
+// 单元测试，必须 Test 开头
+func TestHeapSort(t *testing.T) {
+	var expect = "1,3,4,6,7,9,"
+
+	var values = []int{3, 4, 1, 9, 7, 6}
+	var input string
+	for _, value := range values {
+		input += fmt.Sprintf("%d", value) + ","
+	}
+	println("input: ", input)
+
+	var result, _ = HeapSort(values)
+
+	var output string
+	for _, value := range result {
+		output += fmt.Sprintf("%d", value) + ","
+	}
+	println("output: ", output)
+
+	// 自带的框架没有assert只能通过日志来确定测试是否成功
+	if expect == output {
+        t.Log("heap sort success")
+	} else {
+		t.Error("heap sort failed")
+	}
+}
+
+// 性能测试
+func BenchmarkHeapSort(b *testing.B) {
+	b.StopTimer() // 暂停计时器
+    // TODO 可以做一些前期的准备工作
+	b.StartTimer() // 开启计时器，之前的准备时间未计入总花费时间内
+	for i := 0; i < b.N; i++ {
+		var values = []int{3, 4, 1, 9, 7, 6}
+		HeapSort(values)
+	}
+}
+
