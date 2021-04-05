51c86ee9185b6d5b64b893803c20775fd0263214
korov <korov9@163.com>
2021-04-05 18:05:07 +0800

    add test assert

--- Go/algorithm/src/heap_sort_test.go
+++ Go/algorithm/src/heap_sort_test.go
@@ -3,18 +3,20 @@ package src_test
 import (
 	. "algorithm/src"
 	"fmt"
+	"github.com/go-playground/assert/v2"
 	"testing"
 )
 
 /**
 自带的单元测试框架
- */
+*/
 
 // 单元测试，必须 Test 开头
 func TestHeapSort(t *testing.T) {
 	var logger = GetLogger()
 	logger.Info("start heap sort test")
 	var expect = "1,3,4,6,7,9,"
+	var expectArray = []int{1, 3, 4, 6, 7, 9}
 
 	var values = []int{3, 4, 1, 9, 7, 6}
 	var input string
@@ -31,9 +33,11 @@ func TestHeapSort(t *testing.T) {
 	}
 	println("output: ", output)
 
+	assert.Equal(t, expectArray, result)
+
 	// 自带的框架没有assert只能通过日志来确定测试是否成功
 	if expect == output {
-        t.Log("heap sort success")
+		t.Log("heap sort success")
 	} else {
 		t.Error("heap sort failed")
 	}
@@ -42,11 +46,10 @@ func TestHeapSort(t *testing.T) {
 // 性能测试
 func BenchmarkHeapSort(b *testing.B) {
 	b.StopTimer() // 暂停计时器
-    // TODO 可以做一些前期的准备工作
+	// TODO 可以做一些前期的准备工作
 	b.StartTimer() // 开启计时器，之前的准备时间未计入总花费时间内
 	for i := 0; i < b.N; i++ {
 		var values = []int{3, 4, 1, 9, 7, 6}
 		HeapSort(values)
 	}
 }
-
