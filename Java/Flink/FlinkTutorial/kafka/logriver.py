fa6879b100f4f77acd14f02d5233a5e6993a25e2
korov9 <korov9@163.com>
2021-05-04 00:30:39 +0800

    更新代码

--- Java/Flink/FlinkTutorial/kafka/logriver.py
+++ Java/Flink/FlinkTutorial/kafka/logriver.py
@@ -5,14 +5,13 @@ import time
 client = pymongo.MongoClient("192.168.50.95", 27017)
 db = client.admin
 db.authenticate(name="admin", password="admin", source="admin", mechanism="SCRAM-SHA-256")
-db.logriver
 
 consumer = KafkaConsumer("logriver_siem", bootstrap_servers="192.168.1.19:9092", group_id="siem_test", auto_offset_reset="earliest")
 
 docs = []
 for msg in consumer:
     if len(docs) == 1000 :
-        db.alert.insert_many(docs)
+        db.logriver.insert_many(docs)
         print("%s :insert count %s"%(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), len(docs)))
         docs = []
     else:
--- Java/Flink/FlinkTutorial/kafka/mongo.py
+++ Java/Flink/FlinkTutorial/kafka/mongo.py
@@ -1,7 +1,7 @@
-import pymongo; 
+import pymongo;
 
 client = pymongo.MongoClient("192.168.50.95", 27017)
 db = client.admin
 db.authenticate(name="admin", password="admin", source="admin", mechanism="SCRAM-SHA-256")
-db.alert_test
-db.alert_test.insert_one({"key": "key", "value":"value"}).inserted_id
+id = db.alert_test.insert_one({"key": "key", "value": "value"}).inserted_id
+print(id)
