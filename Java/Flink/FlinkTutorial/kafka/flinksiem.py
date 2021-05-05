2c3f612b5e1416daf1a376ca4b30bbfa01103abf
korov9 <korov9@163.com>
2021-05-05 12:28:42 +0800

    consume kafka

--- Java/Flink/FlinkTutorial/kafka/flinksiem.py
+++ Java/Flink/FlinkTutorial/kafka/flinksiem.py
@@ -6,12 +6,12 @@ client = pymongo.MongoClient("localhost", 27017)
 db = client.admin
 db.authenticate(name="admin", password="admin", source="admin", mechanism="SCRAM-SHA-256")
 
-consumer = KafkaConsumer("logriver_siem", bootstrap_servers="192.168.1.19:9092", group_id="siem_test", auto_offset_reset="earliest")
+consumer = KafkaConsumer("flink_siem", bootstrap_servers="192.168.1.19:9092", group_id="siem_test", auto_offset_reset="earliest")
 
 docs = []
 for msg in consumer:
     if len(docs) == 1 :
-        db.logriver.insert_many(docs)
+        db.alert.insert_many(docs)
         print("%s :insert count %s"%(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), len(docs)))
         docs = []
     else:
