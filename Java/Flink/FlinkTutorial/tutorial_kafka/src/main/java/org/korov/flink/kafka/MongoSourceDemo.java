59976a4503b2038d48be8caad76bcc257cf1fea3
korov9 <korov9@163.com>
2021-05-06 11:53:21 +0800

    完成完善

--- Java/Flink/FlinkTutorial/tutorial_kafka/src/main/java/org/korov/flink/kafka/MongoSourceDemo.java
+++ Java/Flink/FlinkTutorial/tutorial_kafka/src/main/java/org/korov/flink/kafka/MongoSourceDemo.java
@@ -2,7 +2,10 @@ package org.korov.flink.kafka;
 
 import org.apache.flink.api.common.RuntimeExecutionMode;
 import org.apache.flink.api.java.tuple.Tuple3;
+import org.apache.flink.contrib.streaming.state.RocksDBStateBackend;
+import org.apache.flink.streaming.api.CheckpointingMode;
 import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.CheckpointConfig;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.korov.flink.common.sink.MongoSink;
 import org.korov.flink.common.source.MongoSource;
@@ -16,7 +19,7 @@ public class MongoSourceDemo {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.setRuntimeMode(RuntimeExecutionMode.STREAMING);
 
-        /*env.getConfig().setUseSnapshotCompression(true);
+        env.getConfig().setUseSnapshotCompression(true);
         env.getCheckpointConfig().setCheckpointInterval(5 * 60000);
         env.getCheckpointConfig().setCheckpointingMode(CheckpointingMode.EXACTLY_ONCE);
         env.getCheckpointConfig().setMinPauseBetweenCheckpoints(60000);
@@ -26,7 +29,7 @@ public class MongoSourceDemo {
                 CheckpointConfig.ExternalizedCheckpointCleanup.DELETE_ON_CANCELLATION);
 
         RocksDBStateBackend rocksDbStateBackend = new RocksDBStateBackend(args[3], true);
-        env.setStateBackend(rocksDbStateBackend);*/
+        env.setStateBackend(rocksDbStateBackend);
 
         MongoSource mongoSource = new MongoSource("localhost", 27017, "admin", "alert-test");
 
