9d0555e954641977a30f2b94d9edb6dd417839b6
korov <korov9@163.com>
2021-05-24 01:23:27 +0800

    trend not ok

--- web/vue/trend/src/components/Trend.vue
+++ web/vue/trend/src/components/Trend.vue
@@ -1,48 +1,225 @@
 <template>
   <div class="trend">
     <h1>Trend</h1>
-    <TrendChart :datasets="trendData" :grid="gridData" :labels="labelsData" :min="minNumber"
-                interactive="true" @mousemove="onMouseMove">
-    </TrendChart>
+
+    <trend-chart :datasets="datasets" :grid="grid" :interactive="true" :labels="labels" :min="0"
+                class="random-chart" @mouse-move="onMouseMove" id="random-chart"></trend-chart>
+    <div id="pop" ref="tooltip" :class="{'is-active': tooltipData}" class="tooltip" role="tooltip">
+      <div v-if="tooltipData" class="tooltip-container">
+        <strong>{{ labels.xLabels[tooltipData.index] }}</strong>
+        <div class="tooltip-data">
+          <div class="tooltip-data-item tooltip-data-item--1">{{ tooltipData.data[0] }}</div>
+          <div class="tooltip-data-item tooltip-data-item--2">{{ tooltipData.data[1] }}</div>
+          <div class="tooltip-data-item tooltip-data-item--3">{{ tooltipData.data[2] }}</div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import Popper from 'vue-popperjs';
+import 'vue-popperjs/dist/vue-popper.css';
 
 export default {
-  name: 'Trend',
   data() {
     return {
-      trendData: [
+      datasets: [
         {
-          data: [10, 50, 20, 100, 40, 60, 80],
+          data: [70, 100, 400, 180, 100, 300, 500],
           smooth: true,
-          fill: true
+          showPoints: true,
+          fill: true,
+          className: "curve1"
         },
         {
-          data: [40, 50, 60, 500, 400, 600, 800],
+          data: [150, 300, 350, 100, 350, 100, 15],
           smooth: true,
-          fill: true
+          showPoints: true,
+          className: "curve2"
+        },
+        {
+          data: [50, 150, 200, 50, 120, 250, 200],
+          smooth: true,
+          showPoints: true,
+          className: "curve3"
         }
       ],
-      gridData: {
+      grid: {
         verticalLines: true,
         horizontalLines: true
       },
-      labelsData: {
+      labels: {
         xLabels: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
-        yLabels: 5
+        yLabels: 5,
+        yLabelsTextFormatter: val => Math.round(val * 100) / 100
       },
-      minNumber: 0,
-    }
+      tooltipData: null,
+      popper: null,
+      popperIsActive: false
+    };
   },
   methods: {
+    initPopper() {
+      const chart = document.querySelector("#random-chart");
+      const ref = chart.querySelector(".active-line");
+      const tooltip = this.$refs.tooltip;
+      this.popper = new Popper(ref, tooltip, {
+        placement: "right",
+        modifiers: {
+          offset: {offset: "0,10"},
+          preventOverflow: {
+            boundariesElement: chart
+          }
+        }
+      });
+    },
     onMouseMove(params) {
+      this.popperIsActive = !!params;
+      this.popper.scheduleUpdate();
       this.tooltipData = params || null;
     }
   },
-  props: {
-    msg: String
+  mounted() {
+    this.initPopper();
   }
 }
+
 </script>
+
+<style lang="scss" scoped>
+* {
+  box-sizing: border-box;
+}
+
+strong {
+  font-weight: 600;
+}
+
+body {
+  padding: 0;
+  margin: 0;
+  font-family: "Source Sans Pro", sans-serif;
+  color: #2f4053;
+}
+
+#app {
+  margin: 0 auto;
+  padding: 20px;
+  max-width: 600px;
+}
+
+.random {
+  width: 100%;
+
+  .vtc {
+    height: 250px;
+    font-size: 12px;
+    @media (min-width: 699px) {
+      height: 320px;
+    }
+  }
+
+  .labels {
+    stroke: rgba(0, 0, 0, 0.05);
+  }
+
+  .active-line {
+    stroke: rgba(0, 0, 0, 0.2);
+  }
+
+  .point {
+    stroke-width: 2;
+    transition: stroke-width 0.2s;
+  }
+
+  .point.is-active {
+    stroke-width: 5;
+  }
+
+  .curve1 {
+    .stroke {
+      stroke: #fbac91;
+      stroke-width: 2;
+    }
+
+    .fill {
+      fill: #fbac91;
+      opacity: 0.05;
+    }
+
+    .point {
+      fill: #fbac91;
+      stroke: #fbac91;
+    }
+  }
+
+  .curve2 {
+    .stroke {
+      stroke: #fbe1b6;
+      stroke-width: 2;
+    }
+
+    .point {
+      fill: #fbe1b6;
+      stroke: #fbe1b6;
+    }
+  }
+
+  .curve3 {
+    .stroke {
+      stroke: #7fdfd4;
+      stroke-width: 2;
+    }
+
+    .point {
+      fill: #7fdfd4;
+      stroke: #7fdfd4;
+    }
+  }
+
+  .tooltip {
+    &:not(.is-active) {
+      display: none;
+    }
+
+    padding: 10px;
+    background: #fff;
+    box-shadow: 0 0 5px rgba(0, 0, 0, 0.2);
+    pointer-events: none;
+
+    &-data {
+      display: flex;
+
+      &-item {
+        display: flex;
+        align-items: center;
+
+        &:not(:first-child) {
+          margin-left: 20px;
+        }
+
+        &:before {
+          content: "";
+          display: block;
+          width: 15px;
+          height: 15px;
+          margin-right: 5px;
+        }
+
+        &--1:before {
+          background: #fbac91;
+        }
+
+        &--2:before {
+          background: #fbe1b6;
+        }
+
+        &--3:before {
+          background: #7fdfd4;
+        }
+      }
+    }
+  }
+}
+</style>
