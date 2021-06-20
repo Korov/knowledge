c045b18532357c3791ae3cc9ddfe8098ea1097a7
Korov <korov9@163.com>
2021-06-20 18:49:30 +0800

    delete unused import

--- Java/Gradle/algorithms/src/main/java/org/algorithms/example/util/FileIterable.java
+++ Java/Gradle/algorithms/src/main/java/org/algorithms/example/util/FileIterable.java
@@ -8,7 +8,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class FileIterable implements Iterable<int[]> {
     private final String fileName;
