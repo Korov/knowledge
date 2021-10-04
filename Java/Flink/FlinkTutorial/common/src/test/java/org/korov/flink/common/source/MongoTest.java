5ff25184483956870a26c59610018f1197472efe
Korov <korov9@163.com>
2021-10-04 19:49:45 +0800

    update flink

---
+++ Java/Flink/FlinkTutorial/common/src/test/java/org/korov/flink/common/source/MongoTest.java
@@ -0,0 +1,32 @@
+package org.korov.flink.common.source;
+
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientOptions;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import lombok.extern.slf4j.Slf4j;
+import org.bson.Document;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class MongoTest {
+    @Test
+    public void queryBiquge() {
+        ServerAddress serverAddress = new ServerAddress("nas.korov.org", 27017);
+
+        //MongoCredential.createScramSha1Credential()三个参数分别为 用户名 数据库名称 密码
+        MongoCredential credential = MongoCredential.createScramSha256Credential("spider", "spider", "spider".toCharArray());
+
+        MongoClientOptions options = MongoClientOptions.builder().maxConnectionIdleTime(6000).build();
+        //通过连接认证获取MongoDB连接
+        MongoClient mongoClient = new MongoClient(serverAddress, credential, options);
+        MongoDatabase db = mongoClient.getDatabase("spider");
+        MongoCollection<Document> mongoCollection = db.getCollection("book_info");
+        Iterable<Document> documents = mongoCollection.find().limit(10);
+        for (Document document : documents) {
+            log.info("document:{}", document);
+        }
+    }
+}
