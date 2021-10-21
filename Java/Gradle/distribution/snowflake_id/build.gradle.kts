62c71f5ce51318857a4f0b24f49c500fa992a71b
Korov <korov9@163.com>
2021-10-22 00:33:15 +0800

    redis id

--- Java/Gradle/distribution/snowflake_id/build.gradle.kts
+++ Java/Gradle/distribution/snowflake_id/build.gradle.kts
@@ -16,6 +16,7 @@ repositories {
 }
 
 dependencies {
+    implementation("org.redisson:redisson:3.16.3")
     implementation("org.apache.logging.log4j:log4j-api:2.14.1")
     implementation("org.apache.logging.log4j:log4j-core:2.14.1")
     implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.14.1")
