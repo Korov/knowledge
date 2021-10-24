0c0759735bad97eb5dc3b4fa2a22356aeee3b48e
Korov <korov9@163.com>
2021-10-25 00:31:54 +0800

    zookeeper demo

--- Java/Gradle/distribution/id/src/test/java/org/korov/distribution/id/ZookeeperTest.java
+++ Java/Gradle/distribution/id/src/test/java/org/korov/distribution/id/ZookeeperTest.java
@@ -3,19 +3,16 @@ package org.korov.distribution.id;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
-import org.apache.curator.framework.listen.Listenable;
-import org.apache.curator.framework.recipes.cache.ChildData;
 import org.apache.curator.framework.recipes.cache.CuratorCache;
 import org.apache.curator.framework.recipes.cache.CuratorCacheListener;
 import org.apache.curator.framework.recipes.cache.NodeCacheListener;
 import org.apache.curator.retry.ExponentialBackoffRetry;
 import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.data.Stat;
 import org.junit.jupiter.api.Test;
 
 import java.nio.charset.StandardCharsets;
 import java.util.List;
-import java.util.Optional;
-import java.util.stream.Stream;
 
 /**
  * @author zhu.lei
@@ -27,13 +24,50 @@ public class ZookeeperTest {
     void createTest() throws Exception {
         CuratorFramework client = CuratorFrameworkFactory.newClient("localhost:2181", new ExponentialBackoffRetry(10000, 3));
         client.start();
+        String createPath = "/example/test";
 
-        client.delete().forPath("/job-");
-        String path = client.create().forPath("/job-", "test".getBytes(StandardCharsets.UTF_8));
+        Stat pathStat = client.checkExists().forPath(createPath);
+        if (pathStat != null) {
+            log.info("path:{}, stat:{}", createPath, pathStat);
+            client.delete().deletingChildrenIfNeeded().forPath(createPath);
+            log.info("path:{} has been deleted", createPath);
+        } else {
+            log.info("path:{} not exists", createPath);
+        }
+        String path = client.create().creatingParentsIfNeeded().forPath(createPath, "test".getBytes(StandardCharsets.UTF_8));
         log.info("create path:{}", path);
         List<String> paths = client.getChildren().forPath(path);
         log.info("paths:{}", paths);
         byte[] data = client.getData().forPath(path);
+        log.info("data:{} for path:{}", new String(data, StandardCharsets.UTF_8), path);
+    }
+
+    @Test
+    void createTest1() throws Exception {
+        CuratorFramework client = CuratorFrameworkFactory.newClient("localhost:2181", new ExponentialBackoffRetry(10000, 3));
+        client.start();
+        String createPath = "/example1";
+
+        Stat pathStat = client.checkExists().forPath(createPath);
+        if (pathStat != null) {
+            log.info("path:{}, stat:{}", createPath, pathStat);
+            client.delete().deletingChildrenIfNeeded().forPath(createPath);
+            log.info("path:{} has been deleted", createPath);
+        } else {
+            log.info("path:{} not exists", createPath);
+        }
+        String path = client.create().creatingParentsIfNeeded().withMode(CreateMode.EPHEMERAL_SEQUENTIAL).forPath(createPath + "/node-R-", "test".getBytes(StandardCharsets.UTF_8));
+        log.info("create path:{}", path);
+        List<String> paths = client.getChildren().forPath(path);
+        log.info("paths:{}", paths);
+        byte[] data = client.getData().forPath(path);
+        log.info("data:{} for path:{}", new String(data, StandardCharsets.UTF_8), path);
+
+        path = client.create().creatingParentsIfNeeded().withMode(CreateMode.EPHEMERAL_SEQUENTIAL).forPath(createPath + "/node-W-", "test".getBytes(StandardCharsets.UTF_8));
+        log.info("create path:{}", path);
+        paths = client.getChildren().forPath(path);
+        log.info("paths:{}", paths);
+        data = client.getData().forPath(path);
         log.info("data:{} for path:{}", new String(data, StandardCharsets.UTF_8), path);
     }
 
@@ -53,10 +87,10 @@ public class ZookeeperTest {
     }
 
     @Test
-    void watcherTest() {
+    void watcherTest() throws InterruptedException {
         CuratorFramework client = CuratorFrameworkFactory.newClient("localhost:2181", new ExponentialBackoffRetry(10000, 3));
         client.start();
-        CuratorCache curatorCache = CuratorCache.build(client, "/test", CuratorCache.Options.SINGLE_NODE_CACHE);
+        CuratorCache curatorCache = CuratorCache.build(client, "/example1", CuratorCache.Options.SINGLE_NODE_CACHE);
         CuratorCacheListener listener = CuratorCacheListener.builder().forNodeCache(new NodeCacheListener() {
             @Override
             public void nodeChanged() throws Exception {
