9918c4c652d9722058bf43e4e92c95ab052f13c2
Korov <korov9@163.com>
2021-11-07 22:07:49 +0800

    update seata project

--- Java/Gradle/distribution/build.gradle.kts
+++ Java/Gradle/distribution/build.gradle.kts
@@ -9,7 +9,7 @@ ext {
     set("log4jVersion", "2.14.1")
     set("lombokVersion", "1.18.22")
     set("junitVersion", "5.8.1")
-    set("springBootVersion", "2.4.12")
+    set("springBootVersion", "2.3.12.RELEASE")
 }
 
 repositories {
