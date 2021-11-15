e2792777224a7b78dc63f9728da18a71c1727eee
Korov <korov9@163.com>
2021-11-15 22:38:02 +0800

    solution319

---
+++ Java/leetcode/src/main/java/org/leetcode/Solution319.java
@@ -0,0 +1,13 @@
+package org.leetcode;
+
+import java.util.Arrays;
+
+/**
+ * @author zhu.lei
+ * @date 2021-11-15 21:53
+ */
+public class Solution319 {
+    public int bulbSwitch(int n) {
+        return (int) Math.sqrt(n + 0.5);
+    }
+}
