a4ba9e2919828b09eb0b3b6d498506fdf30e4ff4
Korov <korov9@163.com>
2021-11-18 23:49:30 +0800

    add note about spring

--- Java/Flink/FlinkTutorial/kafka/consumer.py
+++ Java/Flink/FlinkTutorial/kafka/consumer.py
@@ -1,6 +1,6 @@
 from util.kafka_consumer import create_consumer, display_topic_partition, consumer_seek, display_all_topics
 
-kafka_consumer = create_consumer(bootstrap_servers="192.168.1.19:9092", group_id="test_group1")
+kafka_consumer = create_consumer(bootstrap_servers="192.168.50.114:9092", group_id="test_group1")
 display_all_topics(kafka_consumer)
 display_topic_partition(kafka_consumer, ["flink_siem"])
 # consumer_seek(kafka_consumer, "flink_siem", 0, 147325460)
