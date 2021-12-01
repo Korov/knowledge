85ec368f1b13934acb6e23812d125ceb95531799
Korov <korov9@163.com>
2021-12-02 01:29:22 +0800

    update tree note

--- Java/Gradle/algorithms/src/main/java/org/algorithms/example/tree/TreeNode.java
+++ Java/Gradle/algorithms/src/main/java/org/algorithms/example/tree/TreeNode.java
@@ -4,11 +4,11 @@ package org.algorithms.example.tree;
  * @author zhu.lei
  * @date 2021-11-26 17:33
  */
-public class TreeNode<K> {
+public class TreeNode<K extends Comparable<K>> {
+    public K value;
     private TreeNode<K> left;
     private TreeNode<K> right;
     private TreeNode<K> parent;
-    public K value;
 
     public TreeNode() {
     }
@@ -17,9 +17,17 @@ public class TreeNode<K> {
         this.value = value;
     }
 
+    public TreeNode<K> getLeft() {
+        return left;
+    }
+
     public void setLeft(TreeNode<K> left) {
         this.left = left;
         left.setParent(this);
+    }
+
+    public TreeNode<K> getRight() {
+        return right;
     }
 
     public void setRight(TreeNode<K> right) {
@@ -27,19 +35,29 @@ public class TreeNode<K> {
         right.setParent(this);
     }
 
+    public TreeNode<K> getParent() {
+        return parent;
+    }
+
     public void setParent(TreeNode<K> parent) {
         this.parent = parent;
     }
 
-    public TreeNode<K> getLeft() {
-        return left;
+    public K getValue() {
+        return value;
     }
 
-    public TreeNode<K> getRight() {
-        return right;
-    }
-
-    public TreeNode<K> getParent() {
-        return parent;
+    public TreeNode<K> treeSearch(TreeNode<K> node, K key) {
+        if (node == null || node.getValue() == null || key == null) {
+            return null;
+        }
+        if (node.getValue().equals(key)) {
+            return node;
+        }
+        if (node.getValue().compareTo(key) > 0) {
+            return treeSearch(node.left, key);
+        } else {
+            return treeSearch(node.right, key);
+        }
     }
 }
--- Java/Gradle/algorithms/src/test/groovy/org/algorithms/example/tree/TreeTest.groovy
+++ Java/Gradle/algorithms/src/test/groovy/org/algorithms/example/tree/TreeTest.groovy
@@ -50,4 +50,40 @@ class TreeTest {
         // GHDBIEFCA
         TreeUtil.inorderTraversal(root)
     }
+
+    @Test
+    void searchTest() {
+        /**
+         *        A
+         *    B       C
+         *  D       E    F
+         * G H        I
+         */
+        TreeNode<String> root = null
+        TreeNode<String> top = new TreeNode<>("D")
+        TreeNode<String> left = new TreeNode<>("G")
+        TreeNode<String> right = new TreeNode<>("H")
+        top.left = left
+        top.right = right
+        left = top
+        top = new TreeNode<>("B")
+        top.left = left
+        left = top
+        top = new TreeNode<>("A")
+        root = top
+        right = new TreeNode<>("C")
+        top.left = left
+        top.right = right
+        top = right
+        left = new TreeNode<>("E")
+        right = new TreeNode<>("F")
+        top.left = left
+        top.right = right
+        top = left
+        right = new TreeNode<>("I")
+        top.right = right
+
+        TreeNode<String> result = root.treeSearch(root, "C")
+        log.info("value:{}", result == null ? "null" : result.getValue().toString())
+    }
 }
