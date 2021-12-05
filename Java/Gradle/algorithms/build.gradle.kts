2fc2766999948db5e5f90f29450f875faa781b34
Korov <korov9@163.com>
2021-12-06 01:12:57 +0800

    update gradle

--- Java/Gradle/algorithms/build.gradle.kts
+++ Java/Gradle/algorithms/build.gradle.kts
@@ -42,13 +42,13 @@ repositories {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_16
-    targetCompatibility = JavaVersion.VERSION_16
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 }
 
 tasks.withType<JavaCompile> {
     options.encoding = "UTF-8"
-    sourceCompatibility = "16"
+    sourceCompatibility = "17"
 }
 
 tasks.withType<Test> {
