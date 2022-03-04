334e3d18aee91bac961e920928452568382d8004
Korov <korov9@163.com>
2022-03-04 15:31:39 +0800

    update note

--- Java/Flink/FlinkTutorial/kafka/consumer.py
+++ Java/Flink/FlinkTutorial/kafka/consumer.py
@@ -1,7 +1,7 @@
 import util.kafka_consumer
 
-kafka_consumer = util.kafka_consumer.create_consumer(bootstrap_servers="192.168.50.163:9092", group_id="monitor")
-# util.kafka_consumer.display_all_topics(kafka_consumer)
-util.kafka_consumer.display_topic_partition(kafka_consumer, ["raw_message"])
+kafka_consumer = util.kafka_consumer.create_consumer(bootstrap_servers="192.168.205.135:30092", group_id="monitor")
+util.kafka_consumer.display_all_topics(kafka_consumer)
+# util.kafka_consumer.display_topic_partition(kafka_consumer, ["raw_message"])
 # util.kafka_consumer.display_topic_consumers(kafka_consumer, ["raw_message"])
 # consumer_seek(kafka_consumer, "raw_message", 0, 1269806324)
