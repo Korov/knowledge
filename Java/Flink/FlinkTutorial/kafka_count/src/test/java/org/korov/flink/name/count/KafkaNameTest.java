4fc388e40926af609830a5be99383a40275582f9
Korov <korov9@163.com>
2022-05-15 23:39:21 +0800

    增加flink的测试用例

--- Java/Flink/FlinkTutorial/kafka_count/src/test/java/org/korov/flink/name/count/KafkaNameTest.java
+++ Java/Flink/FlinkTutorial/kafka_count/src/test/java/org/korov/flink/name/count/KafkaNameTest.java
@@ -1,10 +1,39 @@
 package org.korov.flink.name.count;
 
+import com.google.common.collect.ImmutableList;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.flink.api.common.RuntimeExecutionMode;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Slf4j
 public class KafkaNameTest {
     @Test
-    public void test() {
-        System.out.println("success");
+    public void test() throws Exception {
+        List<String> sourceList = ImmutableList.<String>builder()
+                .add("value1")
+                .add("value2")
+                .build();
+
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setRuntimeMode(RuntimeExecutionMode.STREAMING);
+
+        DataStream<String> stream = env.fromCollection(sourceList);
+        DataStream<Map<String, Long>> mapStream = stream.map(new MapFunction<String, Map<String, Long>>() {
+            @Override
+            public Map<String, Long> map(String value) throws Exception {
+                Map<String, Long> keyTime = new HashMap<>();
+                keyTime.put(value, System.currentTimeMillis());
+                log.info("key:{}, time:{}", value, keyTime.get(value));
+                return keyTime;
+            }
+        });
+        env.execute("map-stream");
     }
 }
