2861db2907e2948de08da34607bdf029e81523f3
Korov <korov9@163.com>
2022-07-29 20:40:18 +0800

    update note about rust

--- rust/hello_world/src/algorithm/algorithm.rs
+++ rust/hello_world/src/algorithm/algorithm.rs
@@ -5,7 +5,44 @@ pub fn test11() {
 }
 
 pub fn valid_square(p1: Vec<i32>, p2: Vec<i32>, p3: Vec<i32>, p4: Vec<i32>) -> bool {
-    info!("p1:{:?}, p2:{:?}, p3:{:?}, p4{:?}", p1, p2, p3, p4);
+    if is_square(&p1, &p2, &p3, &p4) {
+        return true
+    }
+    if is_square(&p1, &p3, &p2, &p4) {
+        return true
+    }
+    if is_square(&p1, &p4, &p2, &p3) {
+        return true
+    }
+    false
+}
+
+pub fn is_square(p1: &Vec<i32>, p2: &Vec<i32>, p3: &Vec<i32>, p4: &Vec<i32>) -> bool {
+    if p1[0] == p2[0] {
+        if p3[0] != p4[0] {
+            return false
+        }
+        
+        if p1[1] == p3[1] {
+            if p2[1] != p4[1] {
+                return false
+            }
+            let length = {p2[1] - p1[1]}.abs();
+            let wide = {p3[0] - p1[0]}.abs();
+            if length == wide {
+                return true
+            }
+        } else {
+            if p2[1] != p3[1] {
+                return false
+            }
+            let length = {p2[1] - p1[1]}.abs();
+            let wide = {p4[0] - p1[0]}.abs();
+            if length == wide {
+                return true
+            }
+        }
+    }
     false
 }
 
@@ -31,12 +68,10 @@ mod test {
     fn test_valid_square() {
         init();
         let p1 = vec![0, 0];
-
         let p2 = vec![1, 1];
-
         let p3 = vec![1, 0];
-
         let p4 = vec![0, 1];
-        valid_square(p1, p2, p3, p4);
+        let result = valid_square(p1, p2, p3, p4);
+        info!("result:{}", result)
     }
 }
