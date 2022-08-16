ccbf66095bb8dcb382b8da0c5c5fa5252e43d55f
Korov <korov9@163.com>
2022-08-16 22:26:38 +0800

    add nav

--- web/vue/vue3_demo/src/App.vue
+++ web/vue/vue3_demo/src/App.vue
@@ -2,27 +2,7 @@
   <div class="common-layout">
     <el-container>
       <el-header class="my-header">
-        <el-menu
-            :default-active="activeIndex"
-            class="el-menu-demo"
-            mode="horizontal"
-            @select="handleSelect">
-          <el-menu-item index="1">Processing Center</el-menu-item>
-          <el-sub-menu index="2">
-            <template #title>Workspace</template>
-            <el-menu-item index="2-1">item one</el-menu-item>
-            <el-menu-item index="2-2">item two</el-menu-item>
-            <el-menu-item index="2-3">item three</el-menu-item>
-            <el-sub-menu index="2-4">
-              <template #title>item four</template>
-              <el-menu-item index="2-4-1">item one</el-menu-item>
-              <el-menu-item index="2-4-2">item two</el-menu-item>
-              <el-menu-item index="2-4-3">item three</el-menu-item>
-            </el-sub-menu>
-          </el-sub-menu>
-          <el-menu-item index="3" disabled>Info</el-menu-item>
-          <el-menu-item index="4">Orders</el-menu-item>
-        </el-menu>
+        <MyMenu/>
       </el-header>
       <el-main class="my-main">Main</el-main>
     </el-container>
@@ -31,49 +11,19 @@
 
 <script lang="ts">
 import {Options, Vue} from 'vue-class-component';
+import MyMenu from "@/components/nav/MyMenu.vue";
 
 @Options({
-  components: {},
+  components: {MyMenu},
 })
 // 变量通过声明属性的方式定义在这里
 export default class App extends Vue {
-  navs = [{
-    title: 'nav1',
-    subNav: [{
-      title: 'subNav1'
-    }, {
-      title: 'subNav2',
-      subNav: [{title: 'subNav21'}
-      ]
-    }]
-  }, {
-    title: 'nav2',
-    subNav: [{
-      title: 'subNav1'
-    }, {
-      title: 'subNav2'
-    }]
-  }]
-  activeIndex = "2-4-2"
-  handleSelect = (key: string, keyPath: string[]) => {
-    this.activeIndex = key
-    console.log(`key:${key}, path:${keyPath}`)
-  }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
 .my-header {
-  /*background: goldenrod;*/
+  background: goldenrod;
 }
 
 .my-main {
---
+++ web/vue/vue3_demo/src/components/nav/MyMenu.vue
@@ -0,0 +1,59 @@
+<template>
+  <el-menu
+      :default-active="activeIndex"
+      class="el-menu-demo"
+      mode="horizontal"
+      @select="handleSelect">
+<!--    <MySubMenu :menuData="menuData"></MySubMenu>-->
+    <el-menu-item v-for="item in menuData" :key="item.id" :index="item.id">
+      <template #title>{{ item.title }}</template>
+      <el-sub-menu v-for="subItem in item.subNav" :key="subItem.id" :index="subItem.id">
+        <template #title>{{ item.title }}</template>
+      </el-sub-menu>
+    </el-menu-item>
+  </el-menu>
+</template>
+
+<script lang="ts">
+import {Options, Vue} from 'vue-class-component';
+import MySubMenu from "@/components/nav/MySubMenu.vue";
+
+@Options({
+  components: {MySubMenu},
+})
+
+// 变量通过声明属性的方式定义在这里
+export default class MyMenu extends Vue {
+  menuData = [{
+    id: 1,
+    title: 'nav1',
+    subNav: [{
+      id: 11,
+      title: 'subNav1'
+    }, {
+      id: 12,
+      title: 'subNav2',
+      subNav: [{title: 'subNav21'}
+      ]
+    }]
+  }, {
+    id: 2,
+    title: 'nav2',
+    subNav: [{
+      id: 21,
+      title: 'subNav1'
+    }, {
+      id: 22,
+      title: 'subNav2'
+    }]
+  }]
+  activeIndex = "1"
+  handleSelect = (key: string, keyPath: string[]) => {
+    this.activeIndex = key
+    console.log(`key:${key}, path:${keyPath}`)
+  }
+}
+</script>
+
+<style>
+</style>
