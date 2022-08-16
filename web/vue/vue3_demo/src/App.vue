5a31d3df8c6e358867bb28daea488d348e426146
Korov <korov9@163.com>
2022-08-17 01:02:18 +0800

    add nav

--- web/vue/vue3_demo/src/App.vue
+++ web/vue/vue3_demo/src/App.vue
@@ -2,23 +2,40 @@
   <div class="common-layout">
     <el-container>
       <el-header class="my-header">
-        <MyMenu/>
+        <MyMenu :menuData="menuData"></MyMenu>
       </el-header>
       <el-main class="my-main">Main</el-main>
     </el-container>
   </div>
 </template>
 
-<script lang="ts">
-import {Options, Vue} from 'vue-class-component';
-import MyMenu from "@/components/nav/MyMenu.vue";
+<script setup lang="ts">
+import MyMenu from './components/nav/MyMenu'
 
-@Options({
-  components: {MyMenu},
-})
-// 变量通过声明属性的方式定义在这里
-export default class App extends Vue {
-}
+let menuData = [{
+  id: "1",
+  title: 'nav1',
+  subNav: [{
+    id: "11",
+    title: 'subNav1'
+  }, {
+    id: "12",
+    title: 'subNav2',
+    subNav: [{title: 'subNav21'}
+    ]
+  }]
+}, {
+  id: "2",
+  title: 'nav2',
+  subNav: [{
+    id: "21",
+    title: 'subNav1'
+  }, {
+    id: "22",
+    title: 'subNav2'
+  }]
+}]
+
 </script>
 
 <style>
--- web/vue/vue3_demo/src/components/nav/MyMenu.vue
+++ web/vue/vue3_demo/src/components/nav/MyMenu.vue
@@ -4,7 +4,7 @@
       class="el-menu-demo"
       mode="horizontal"
       @select="handleSelect">
-    <el-sub-menu v-for="item in menuData" :key="item.id" :index="item.id">
+    <el-sub-menu v-for="item in props.menuData" :key="item.id" :index="item.id">
       <template #title>{{ item.title }}</template>
       <el-menu-item v-for="subItem in item.subNav" :key="subItem.id" :index="subItem.id">
         <template #title>{{ item.title }}</template>
@@ -13,44 +13,17 @@
   </el-menu>
 </template>
 
-<script lang="ts">
-import {Options, Vue} from 'vue-class-component';
-import MySubMenu from "@/components/nav/MySubMenu.vue";
+<script setup lang="ts">
+import {defineProps} from "vue";
 
-@Options({
-  components: {MySubMenu},
+let props = defineProps({
+  menuData: {}
 })
+let activeIndex = "11"
 
-// 变量通过声明属性的方式定义在这里
-export default class MyMenu extends Vue {
-  menuData = [{
-    id: 1,
-    title: 'nav1',
-    subNav: [{
-      id: 11,
-      title: 'subNav1'
-    }, {
-      id: 12,
-      title: 'subNav2',
-      subNav: [{title: 'subNav21'}
-      ]
-    }]
-  }, {
-    id: 2,
-    title: 'nav2',
-    subNav: [{
-      id: 21,
-      title: 'subNav1'
-    }, {
-      id: 22,
-      title: 'subNav2'
-    }]
-  }]
-  activeIndex = "1"
-  handleSelect = (key: string, keyPath: string[]) => {
-    this.activeIndex = key
-    console.log(`key:${key}, path:${keyPath}`)
-  }
+function handleSelect(key: string, keyPath: string[]) {
+  activeIndex = key
+  console.log(`key:${key}, path:${keyPath}`)
 }
 </script>
 
