3942bace196e9af3fc21c85fab7cde101d50e6b9
Korov <korov9@163.com>
2022-08-16 20:24:42 +0800

    add nav

--- web/vue/vue3_demo/src/App.vue
+++ web/vue/vue3_demo/src/App.vue
@@ -2,14 +2,27 @@
   <div class="common-layout">
     <el-container>
       <el-header class="my-header">
-        <el-button
-            v-for="button in buttons"
-            :key="button.text"
-            :type="button.type"
-            :disabled="button.disabled"
-            link
-        >{{ button.text }}
-        </el-button>
+        <el-menu
+            :default-active="activeIndex"
+            class="el-menu-demo"
+            mode="horizontal"
+            @select="handleSelect">
+          <el-menu-item index="1">Processing Center</el-menu-item>
+          <el-sub-menu index="2">
+            <template #title>Workspace</template>
+            <el-menu-item index="2-1">item one</el-menu-item>
+            <el-menu-item index="2-2">item two</el-menu-item>
+            <el-menu-item index="2-3">item three</el-menu-item>
+            <el-sub-menu index="2-4">
+              <template #title>item four</template>
+              <el-menu-item index="2-4-1">item one</el-menu-item>
+              <el-menu-item index="2-4-2">item two</el-menu-item>
+              <el-menu-item index="2-4-3">item three</el-menu-item>
+            </el-sub-menu>
+          </el-sub-menu>
+          <el-menu-item index="3" disabled>Info</el-menu-item>
+          <el-menu-item index="4">Orders</el-menu-item>
+        </el-menu>
       </el-header>
       <el-main class="my-main">Main</el-main>
     </el-container>
@@ -24,14 +37,28 @@ import {Options, Vue} from 'vue-class-component';
 })
 // 变量通过声明属性的方式定义在这里
 export default class App extends Vue {
-  buttons = [
-    {type: '', text: 'plain', disabled: false},
-    {type: 'primary', text: 'primary', disabled: true},
-    {type: 'success', text: 'success', disabled: false},
-    {type: 'info', text: 'info', disabled: false},
-    {type: 'warning', text: 'warning', disabled: false},
-    {type: 'danger', text: 'danger', disabled: false},
-  ] as const
+  navs = [{
+    title: 'nav1',
+    subNav: [{
+      title: 'subNav1'
+    }, {
+      title: 'subNav2',
+      subNav: [{title: 'subNav21'}
+      ]
+    }]
+  }, {
+    title: 'nav2',
+    subNav: [{
+      title: 'subNav1'
+    }, {
+      title: 'subNav2'
+    }]
+  }]
+  activeIndex = "2-4-2"
+  handleSelect = (key: string, keyPath: string[]) => {
+    this.activeIndex = key
+    console.log(`key:${key}, path:${keyPath}`)
+  }
 }
 </script>
 
