b58be26d609d80a4b67a900c233c48aa8b3f1a05
Korov <korov9@163.com>
2022-08-18 18:30:17 +0800

    add web

---
+++ Java/SpringBoot/demo/src/main/java/org/korov/demo/controller/DemoController.java
@@ -0,0 +1,18 @@
+package org.korov.demo.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    private static final Logger log = LoggerFactory.getLogger(DemoController.class);
+
+    @GetMapping(path = "/hello")
+    public String hello(@RequestParam("name") String name) {
+        log.info("hello {}", name);
+        return String.format("hello %s", name);
+    }
+}
