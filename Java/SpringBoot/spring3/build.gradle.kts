d4842a5853c8fea96acc1ceccedaa8f978f794d8
Korov <korov9@163.com>
2022-09-19 10:34:12 +0800

    migrate to kts

---
+++ Java/SpringBoot/spring3/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+	id("org.springframework.boot") version("3.0.0-M4")
+	id("io.spring.dependency-management") version("1.0.13.RELEASE")
+	id("java")
+}
+
+group = "org.korov"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_17
+	targetCompatibility = JavaVersion.VERSION_17
+}
+
+repositories {
+	mavenCentral()
+	maven { url = uri("https://repo.spring.io/milestone") }
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	runtimeOnly("org.postgresql:postgresql")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.getByName<Test>("test") {
+	useJUnitPlatform()
+}
