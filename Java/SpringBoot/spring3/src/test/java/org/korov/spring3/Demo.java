b29d425a6eb2212429c3d6b05e27cd01d534f787
Korov <korov9@163.com>
2022-10-05 13:57:48 +0800

    upgrade springboot

--- Java/SpringBoot/spring3/src/test/java/org/korov/spring3/Demo.java
+++ Java/SpringBoot/spring3/src/test/java/org/korov/spring3/Demo.java
@@ -3,11 +3,13 @@ package org.korov.spring3;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class Demo {
 
     @Test
     public void test1() {
         Map<Integer, String> map = new HashMap<>();
+        map.put(1, "1");
     }
 }
