878393a33a399db76c49af9e2395a2472506b0d6
Korov <korov9@163.com>
2022-10-15 02:55:50 +0800

    perf solution

--- Java/Gradle/algorithms/src/main/java/org/algorithms/example/solution/Solutions801.java
+++ Java/Gradle/algorithms/src/main/java/org/algorithms/example/solution/Solutions801.java
@@ -2,21 +2,20 @@ package org.algorithms.example.solution;
 
 public class Solutions801 {
     public static int minSwap(int[] nums1, int[] nums2) {
-        int[][] dp = new int[nums1.length][2];
-        dp[0][0] = 0;
-        dp[0][1] = 1;
+        int previous1 = 0;
+        int previous2 = 1;
         for (int i = 1; i < nums1.length; i++) {
             if ((nums1[i] > nums1[i - 1] && nums2[i] > nums2[i - 1]) && (nums1[i] > nums2[i - 1] && nums2[i] > nums1[i - 1])) {
-                dp[i][0] = Math.min(dp[i - 1][0], dp[i - 1][1]);
-                dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][1]) + 1;
+                previous1 = Math.min(previous1, previous2);
+                previous2 = previous1 + 1;
             } else if (nums1[i] > nums1[i - 1] && nums2[i] > nums2[i - 1]) {
-                dp[i][0] = dp[i - 1][0];
-                dp[i][1] = dp[i - 1][1] + 1;
+                previous2++;
             } else {
-                dp[i][0] = dp[i - 1][1];
-                dp[i][1] = dp[i - 1][0] + 1;
+                int temp = previous1;
+                previous1 = previous2;
+                previous2 = temp + 1;
             }
         }
-        return Math.min(dp[nums1.length - 1][0], dp[nums1.length - 1][1]);
+        return Math.min(previous1, previous2);
     }
 }
