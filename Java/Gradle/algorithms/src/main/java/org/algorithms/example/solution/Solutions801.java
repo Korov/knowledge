65438241a18cffff6c37ebe53f7d28958b121198
Korov <korov9@163.com>
2022-10-15 02:18:48 +0800

    get solution

--- Java/Gradle/algorithms/src/main/java/org/algorithms/example/solution/Solutions801.java
+++ Java/Gradle/algorithms/src/main/java/org/algorithms/example/solution/Solutions801.java
@@ -2,19 +2,21 @@ package org.algorithms.example.solution;
 
 public class Solutions801 {
     public static int minSwap(int[] nums1, int[] nums2) {
-        int[] min = new int[nums1.length];
-        min[0] = 0;
-        int temp;
+        int[][] dp = new int[nums1.length][2];
+        dp[0][0] = 0;
+        dp[0][1] = 1;
         for (int i = 1; i < nums1.length; i++) {
-            if (nums1[i] > nums1[i - 1] && nums2[i] > nums2[i - 1]) {
-                min[i] = min[i - 1];
+            if ((nums1[i] > nums1[i - 1] && nums2[i] > nums2[i - 1]) && (nums1[i] > nums2[i - 1] && nums2[i] > nums1[i - 1])) {
+                dp[i][0] = Math.min(dp[i - 1][0], dp[i - 1][1]);
+                dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][1]) + 1;
+            } else if (nums1[i] > nums1[i - 1] && nums2[i] > nums2[i - 1]) {
+                dp[i][0] = dp[i - 1][0];
+                dp[i][1] = dp[i - 1][1] + 1;
             } else {
-                temp = nums1[i];
-                nums1[i] = nums2[i];
-                nums2[i] = temp;
-                min[i] = min[i - 1] + 1;
+                dp[i][0] = dp[i - 1][1];
+                dp[i][1] = dp[i - 1][0] + 1;
             }
         }
-        return min[nums1.length - 1];
+        return Math.min(dp[nums1.length - 1][0], dp[nums1.length - 1][1]);
     }
 }
--- Java/Gradle/algorithms/src/test/groovy/org/algorithms/example/solution/Solutions801Test.groovy
+++ Java/Gradle/algorithms/src/test/groovy/org/algorithms/example/solution/Solutions801Test.groovy
@@ -16,4 +16,28 @@ class Solutions801Test extends Specification {
         then:
         result == 1
     }
+
+    def "test1"() {
+        given:
+        def num1 = [0, 1, 4, 6, 8] as int[]
+        def num2 = [1, 2, 2, 7, 10] as int[]
+
+        when:
+        def result = Solutions801.minSwap(num1, num2)
+
+        then:
+        result == 1
+    }
+
+    def "test2"() {
+        given:
+        def num1 = [0, 7, 8, 10, 10, 11, 12, 13, 19, 18] as int[]
+        def num2 = [4, 4, 5, 7, 11, 14, 15, 16, 17, 20] as int[]
+
+        when:
+        def result = Solutions801.minSwap(num1, num2)
+
+        then:
+        result == 4
+    }
 }
