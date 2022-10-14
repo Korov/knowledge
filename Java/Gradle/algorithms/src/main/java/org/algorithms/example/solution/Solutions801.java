5a665a695b6ea06fc0a823c8af76b18641e669d4
Korov <korov9@163.com>
2022-10-15 03:18:58 +0800

    perf solution

--- Java/Gradle/algorithms/src/main/java/org/algorithms/example/solution/Solutions801.java
+++ Java/Gradle/algorithms/src/main/java/org/algorithms/example/solution/Solutions801.java
@@ -5,15 +5,15 @@ public class Solutions801 {
         int previous1 = 0;
         int previous2 = 1;
         for (int i = 1; i < nums1.length; i++) {
-            if ((nums1[i] > nums1[i - 1] && nums2[i] > nums2[i - 1]) && (nums1[i] > nums2[i - 1] && nums2[i] > nums1[i - 1])) {
-                previous1 = Math.min(previous1, previous2);
-                previous2 = previous1 + 1;
-            } else if (nums1[i] > nums1[i - 1] && nums2[i] > nums2[i - 1]) {
+            if ((nums1[i] > nums1[i - 1] && nums2[i] > nums2[i - 1]) && (nums1[i] <= nums2[i - 1] || nums2[i] <= nums1[i - 1])) {
                 previous2++;
-            } else {
+            } else if (nums1[i] <= nums1[i - 1] || nums2[i] <= nums2[i - 1]) {
                 int temp = previous1;
                 previous1 = previous2;
                 previous2 = temp + 1;
+            } else {
+                previous1 = Math.min(previous1, previous2);
+                previous2 = previous1 + 1;
             }
         }
         return Math.min(previous1, previous2);
