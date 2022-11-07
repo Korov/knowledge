78dc5f668c52e522fc75d92f454a2d0f3955c193
Korov <korov9@163.com>
2022-11-07 18:35:49 +0800

    trim sql

--- Python/Study/excel/ExtractTacticsSql.py
+++ Python/Study/excel/ExtractTacticsSql.py
@@ -11,9 +11,9 @@ if __name__ == "__main__":
                     cells.append((cell.min_col, cell.min_row, cell.max_row))
 
         cells.sort()
-        tactics_code = worksheet.cell(column=1, row=3).value
-        tactics_name = worksheet.cell(column=2, row=3).value
-        tactics_description = worksheet.cell(column=3, row=3).value
+        tactics_code = worksheet.cell(column=1, row=3).value.strip()
+        tactics_name = worksheet.cell(column=2, row=3).value.strip()
+        tactics_description = worksheet.cell(column=3, row=3).value.strip()
         print(
             "('{}', '', 0, '{}', '{}', '', '', NOW(), NOW()),".format(tactics_code, tactics_name, tactics_description))
         for cell in cells:
@@ -21,21 +21,21 @@ if __name__ == "__main__":
             min_row = cell[1]
             max_row = cell[2]
 
-            techniques_code = worksheet.cell(column=col, row=min_row).value
-            techniques_name = worksheet.cell(column=col + 1, row=min_row).value
-            techniques_description = worksheet.cell(column=col + 2, row=min_row).value
+            techniques_code = worksheet.cell(column=col, row=min_row).value.strip()
+            techniques_name = worksheet.cell(column=col + 1, row=min_row).value.strip()
+            techniques_description = worksheet.cell(column=col + 2, row=min_row).value.strip()
             print("('{}', '{}', 1, '{}', '{}', '', '', NOW(), NOW()),".format(techniques_code, tactics_code,
                                                                               techniques_name, tactics_description))
 
             for row_index in range(min_row, max_row + 1):
-                sub_techniques_code = worksheet.cell(column=col + 3, row=row_index).value
-                sub_techniques_name = worksheet.cell(column=col + 4, row=row_index).value
-                sub_techniques_description = worksheet.cell(column=col + 5, row=row_index).value
+                sub_techniques_code = worksheet.cell(column=col + 3, row=row_index).value.strip()
+                sub_techniques_name = worksheet.cell(column=col + 4, row=row_index).value.strip()
+                sub_techniques_description = worksheet.cell(column=col + 5, row=row_index).value.strip()
                 print("('{}', '{}', 2, '{}', '{}', '', '', NOW(), NOW()),".format(sub_techniques_code, techniques_code,
                                                                                   sub_techniques_name,
                                                                                   sub_techniques_description))
 
-        print("\n\n\n\n")
+        print("\n\n")
 
     # print('workbook title:{}, max column:{}, max row:{}'.format(worksheet.title, worksheet.max_column,
     #                                                             worksheet.max_row))
