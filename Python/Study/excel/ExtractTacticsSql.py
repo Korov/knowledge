e7770ad0da63948a12c100d33cb8e42efe25a60e
Korov <korov9@163.com>
2022-11-09 10:38:28 +0800

    update python script

--- Python/Study/excel/ExtractTacticsSql.py
+++ Python/Study/excel/ExtractTacticsSql.py
@@ -3,6 +3,7 @@ import openpyxl
 if __name__ == "__main__":
     originFile = 'C:\\Users\\korov\\Downloads\\MITRE ATT&CK   矩阵技术描述列表（中文版)_20220919 - Copy.xlsx'
     workbook = openpyxl.load_workbook(originFile)
+    techniques_map = dict()
     for worksheet in workbook.worksheets:
         cells = []
         for cell in worksheet.merged_cells.ranges:
@@ -14,7 +15,8 @@ if __name__ == "__main__":
         tactics_code = worksheet.cell(column=1, row=3).value.strip()
         tactics_name = worksheet.cell(column=2, row=3).value.strip()
         tactics_description = worksheet.cell(column=3, row=3).value.strip()
-        print('INSERT INTO `siem_att_ck_info`(`code`, `parent_code`, `type`, `name_zh`, `description_zh`, `name_en`, `description_en`, `create_time`, `update_time`) VALUES')
+        print(
+            'INSERT INTO `siem_att_ck_info`(`code`, `parent_code`, `type`, `name_zh`, `description_zh`, `name_en`, `description_en`, `create_time`, `update_time`) VALUES')
         print(
             "('{}', '', 0, '{}', '{}', '', '', NOW(), NOW()),".format(tactics_code, tactics_name, tactics_description))
 
@@ -27,8 +29,22 @@ if __name__ == "__main__":
             techniques_code = worksheet.cell(column=col, row=min_row).value.strip()
             techniques_name = worksheet.cell(column=col + 1, row=min_row).value.strip()
             techniques_description = worksheet.cell(column=col + 2, row=min_row).value.strip()
-            print("('{}', '{}', 1, '{}', '{}', '', '', NOW(), NOW()),".format(techniques_code, tactics_code,
-                                                                              techniques_name, tactics_description))
+            if techniques_map.keys().__contains__(techniques_code):
+                techniques_map[techniques_code].append(tactics_code)
+                if cell_index == len(cells) - 1:
+                    print("('{}', '{}', 1, '{}', '{}', '', '', NOW(), NOW());".format(techniques_code, tactics_code,
+                                                                                      techniques_name,
+                                                                                      tactics_description))
+                else:
+                    print("('{}', '{}', 1, '{}', '{}', '', '', NOW(), NOW()),".format(techniques_code, tactics_code,
+                                                                                      techniques_name,
+                                                                                      tactics_description))
+                continue
+            else:
+                techniques_map[techniques_code] = [tactics_code]
+
+                print("('{}', '{}', 1, '{}', '{}', '', '', NOW(), NOW()),".format(techniques_code, tactics_code,
+                                                                                  techniques_name, tactics_description))
 
             for row_index in range(min_row, max_row + 1):
                 sub_techniques_code = worksheet.cell(column=col + 3, row=row_index).value.strip()
@@ -37,9 +53,9 @@ if __name__ == "__main__":
 
                 if row_index == max_row and cell_index == len(cells) - 1:
                     print("('{}', '{}', 2, '{}', '{}', '', '', NOW(), NOW());".format(sub_techniques_code,
-                                                                                  techniques_code,
-                                                                                  sub_techniques_name,
-                                                                                  sub_techniques_description))
+                                                                                      techniques_code,
+                                                                                      sub_techniques_name,
+                                                                                      sub_techniques_description))
                 else:
                     print("('{}', '{}', 2, '{}', '{}', '', '', NOW(), NOW()),".format(sub_techniques_code,
                                                                                       techniques_code,
@@ -47,6 +63,8 @@ if __name__ == "__main__":
                                                                                       sub_techniques_description))
 
         print("\n\n")
+
+    print("finish")
 
     # print('workbook title:{}, max column:{}, max row:{}'.format(worksheet.title, worksheet.max_column,
     #                                                             worksheet.max_row))
