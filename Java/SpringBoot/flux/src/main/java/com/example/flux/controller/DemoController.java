4bc30e6df71ea9ed78dc08bd9db3206e5e425374
Korov <korov9@163.com>
2022-11-19 23:57:16 +0800

    deep fathom about webflux

--- Java/SpringBoot/flux/src/main/java/com/example/flux/controller/DemoController.java
+++ Java/SpringBoot/flux/src/main/java/com/example/flux/controller/DemoController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.*;
 import reactor.core.CoreSubscriber;
 import reactor.core.publisher.Mono;
 
+import java.util.concurrent.TimeUnit;
+
 @RestController
 public class DemoController {
     private static final Logger log = LoggerFactory.getLogger(DemoController.class);
@@ -13,6 +15,17 @@ public class DemoController {
     @RequestMapping(value = "/echo/{string}", method = RequestMethod.GET)
     public Mono<String> echo(@PathVariable String string) {
         log.info("provider get str:{}", string);
-        return Mono.just(String.format("get user %s", string));
+        // Mono<String> anotherResult = Mono.just(String.format("get user %s", string));
+        Mono<String> result = Mono.fromSupplier(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(5);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            return String.format("get user %s", string);
+        });
+        // 这个Mono是立即返回，没有等待5秒钟，这时这个线程可以去做其他的事情
+        log.info("provider return mono");
+        return result;
     }
 }
