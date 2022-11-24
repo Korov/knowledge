314e044371812285ef508d71954c85e9a0cd1ce1
Korov <korov9@163.com>
2022-11-24 20:02:53 +0800

    delete unused project

--- web/vue/vue3_demo/src/http-common.ts
+++
@@ -1,9 +0,0 @@
-import axios, { AxiosInstance } from "axios";
-const apiClient: AxiosInstance = axios.create({
-    baseURL: "http://localhost:6060",
-    headers: {
-        "Content-type": "application/json",
-        // "Access-Control-Allow-Origin": "*",
-    },
-});
-export default apiClient;
