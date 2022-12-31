4b449190c0e02db27b3a16f39097e157cd23d449
Korov <korov9@163.com>
2022-12-31 13:52:55 +0800

    update project

--- Java/SpringCloud/alicloud-demo/provider/src/main/java/com/korov/alicloud/provider/controller/EchoController.java
+++ Java/SpringCloud/alicloud-demo/provider/src/main/java/com/korov/alicloud/provider/controller/EchoController.java
@@ -4,12 +4,17 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * @RefreshScope 可以自动刷新属性的值
+ */
+@RefreshScope
 @RestController
 public class EchoController {
     private static final Logger log = LoggerFactory.getLogger(EchoController.class);
@@ -28,8 +33,8 @@ public class EchoController {
     @RequestMapping(value = "/echo/{string}", method = RequestMethod.GET)
     public String echo(@PathVariable String string) {
         // 当动态配置刷新时，会更新到 Environment 中，因此需要手动获取更新之后的值
-        configName = applicationContext.getEnvironment().getProperty("config.name");
+        // configName = applicationContext.getEnvironment().getProperty("config.name");
         log.info("provider get str:{}, and config name:{}", string, configName);
-        return "Hello Nacos Discovery " + string;
+        return String.format("Hello Nacos Discovery %s, configName:%s", string, configName);
     }
 }
