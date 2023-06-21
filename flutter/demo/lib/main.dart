4cb4b3d1d33579e167ae532a9a2687ad6c023f13
Korov <korov9@163.com>
2023-06-21 11:46:05 +0800

    update project

--- flutter/demo/lib/main.dart
+++ flutter/demo/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'components/MyDrawer.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -99,7 +101,7 @@ class _MyHomePageState extends State<MyHomePage> {
           );
         }),
       ),
-      drawer: MyDrawer(), //抽屉
+      drawer: MenuDrawer(), //抽屉
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
@@ -152,61 +154,6 @@ class _MyHomePageState extends State<MyHomePage> {
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}
-
-class MyDrawer extends StatelessWidget {
-  const MyDrawer({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Drawer(
-      child: MediaQuery.removePadding(
-        context: context,
-        //移除抽屉菜单顶部默认留白
-        removeTop: true,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            const Padding(
-              padding: EdgeInsets.only(top: 38.0),
-              child: Row(
-                children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 16.0),
-                    child: ClipOval(
-                      child: Text(
-                        "add images",
-                      ),
-                    ),
-                  ),
-                  Text(
-                    "Wendux",
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  )
-                ],
-              ),
-            ),
-            Expanded(
-              child: ListView(
-                children: const <Widget>[
-                  ListTile(
-                    leading: Icon(Icons.add),
-                    title: Text('Add account'),
-                  ),
-                  ListTile(
-                    leading: Icon(Icons.settings),
-                    title: Text('Manage accounts'),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
