c2341d83b0b96124f588dd73d457f752b7936c79
Korov <korov9@163.com>
2023-08-31 10:52:20 +0800

    update project

--- PyTorch/Demo/tests/LinearRegressionImplementation.py
+++ PyTorch/Demo/tests/LinearRegressionImplementation.py
@@ -1,15 +1,98 @@
-import torch
-from IPython import display
-from matplotlib import pyplot as plt
-import numpy as np
 import random
 
-num_inputs = 2
-num_examples = 1000
-true_w = [2, -3.4]
-true_b = 4.2
-features = torch.from_numpy(np.random.normal(0, 1, (num_examples, num_inputs)))
-labels = true_w[0] * features[:, 0] + true_w[1] * features[:, 1] + true_b
-labels += torch.from_numpy(np.random.normal(0, 0.01, size=labels.size()))
+import matplotlib_inline
+import torch
+import numpy as np
+from matplotlib import pyplot as plt
 
-print(features[0], labels[0])
+
+def use_svg_display():
+    """用矢量图显示"""
+    matplotlib_inline.backend_inline.set_matplotlib_formats('svg')
+
+
+def set_figsize(figsize=(3.5, 2.5)):
+    use_svg_display()
+    # 设置图的尺寸
+    plt.rcParams['figure.figsize'] = figsize
+
+
+def data_iter(batch_size, features, labels):
+    """在训练模型的时候，我们需要遍历数据集并不断读取小批量数据样本。这里我们定义一个函数：它每次返回`batch_size`（批量大小）个随机样本的特征和标签。"""
+    num_examples = len(features)
+    indices = list(range(num_examples))
+    random.shuffle(indices)  # 样本的读取顺序是随机的
+    for i in range(0, num_examples, batch_size):
+        j = torch.LongTensor(indices[i: min(i + batch_size, num_examples)])  # 最后一次可能不足一个batch
+        yield features.index_select(0, j), labels.index_select(0, j)
+
+
+def linreg(X, w, b):
+    """下面是线性回归的矢量计算表达式的实现。我们使用`mm`函数做矩阵乘法"""
+    return torch.mm(X, w) + b
+
+def squared_loss(y_hat, y):
+    """我们使用上一节描述的平方损失来定义线性回归的损失函数。在实现中，我们需要把真实值`y`变形成预测值`y_hat`的形状。以下函数返回的结果也将和`y_hat`的形状相同。"""
+    # 注意这里返回的是向量, 另外, pytorch里的MSELoss并没有除以 2
+    return (y_hat - y.view(y_hat.size())) ** 2 / 2
+
+def sgd(params, lr, batch_size):
+    """以下的`sgd`函数实现了上一节中介绍的小批量随机梯度下降算法。它通过不断迭代模型参数来优化损失函数。这里自动求梯度模块计算得来的梯度是一个批量样本的梯度和。我们将它除以批量大小来得到平均值。"""
+    for param in params:
+        param.data -= lr * param.grad / batch_size # 注意这里更改param时用的param.data
+
+if __name__ == "__main__":
+    num_inputs = 2
+    num_examples = 1000
+    true_w = [2, -3.4]
+    true_b = 4.2
+    features = torch.randn(num_examples, num_inputs,
+                           dtype=torch.float32)
+    labels = true_w[0] * features[:, 0] + true_w[1] * features[:, 1] + true_b
+    labels += torch.tensor(np.random.normal(0, 0.01, size=labels.size()),
+                           dtype=torch.float32)
+
+    print(features[0], labels[0])
+
+    set_figsize()
+    plt.scatter(features[:, 1].numpy(), labels.numpy(), 1)
+    plt.show()
+
+    batch_size = 10
+
+    # 让我们读取第一个小批量数据样本并打印。每个批量的特征形状为(10, 2)，分别对应批量大小和输入个数；标签形状为批量大小
+    for X, y in data_iter(batch_size, features, labels):
+        print(X, y)
+        break
+
+    # 我们将权重初始化成均值为0、标准差为0.01的正态随机数，偏差则初始化成0。
+    w = torch.tensor(np.random.normal(0, 0.01, (num_inputs, 1)), dtype=torch.float32)
+    b = torch.zeros(1, dtype=torch.float32)
+
+    # 之后的模型训练中，需要对这些参数求梯度来迭代参数的值，因此我们要让它们的`requires_grad=True`。
+    w.requires_grad_(requires_grad=True)
+    b.requires_grad_(requires_grad=True)
+
+
+    # 训练模型
+    lr = 0.03
+    num_epochs = 3
+    net = linreg
+    loss = squared_loss
+
+    for epoch in range(num_epochs):  # 训练模型一共需要num_epochs个迭代周期
+        # 在每一个迭代周期中，会使用训练数据集中所有样本一次（假设样本数能够被批量大小整除）。X
+        # 和y分别是小批量样本的特征和标签
+        for X, y in data_iter(batch_size, features, labels):
+            l = loss(net(X, w, b), y).sum()  # l是有关小批量X和y的损失
+            l.backward()  # 小批量的损失对模型参数求梯度
+            sgd([w, b], lr, batch_size)  # 使用小批量随机梯度下降迭代模型参数
+
+            # 不要忘了梯度清零
+            w.grad.data.zero_()
+            b.grad.data.zero_()
+        train_l = loss(net(features, w, b), labels)
+        print('epoch %d, loss %f' % (epoch + 1, train_l.mean().item()))
+
+    print(true_w, '\n', w)
+    print(true_b, '\n', b)
